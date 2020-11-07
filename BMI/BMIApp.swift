4d96a134621c68b96b8a83cfa27f5edd9b797048
Hidetoshi Kato <tline1059@icloud.com>
2020-11-07 20:48:50 +0900

    Initial Commit

---
+++ BMI/BMIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BMIApp.swift
+//  BMI
+//
+//  Created by D-MacBookPro13 on 2020/11/07.
+//
+
+import SwiftUI
+
+@main
+struct BMIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BMI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BMI
+//
+//  Created by D-MacBookPro13 on 2020/11/07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
